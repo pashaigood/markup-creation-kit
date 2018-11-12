03b151eb4cd8d5026b8a7af14f2fba264300a345
Pavel Belugin <pashaigood@gmail.com>
2018-11-12 20:39:14 +0300

    feat(resources): Implemented favicon

--- webpack/resources.js
+++ webpack/resources.js
@@ -1,28 +1,52 @@
+const Paths = require('./constants/Paths');
+const path = require('path');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+
 module.exports = () => {
   return {
     module: {
       rules: [
         {
           test: /\.(woff|woff2|eot|ttf|otf)$/,
-          use: [{
-            loader: 'file-loader',
-            options: {
-              outputPath: 'fonts/',
-              name: '[name].[ext]'
-            }
-          }],
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                outputPath: 'fonts/',
+                name: '[name].[ext]',
+              },
+            }],
         },
         {
           test: /\.(png|svg|jpg|gif)$/,
-          use: [{
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'images/'
-            }
-          }],
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[name].[ext]',
+                outputPath: 'images/',
+              },
+            }],
         },
       ],
     },
+    plugins: [
+      new FaviconsWebpackPlugin({
+        prefix: 'images/logo/',
+        logo: path.join(Paths.source, 'images', 'favicon.jpg'),
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          opengraph: false,
+          twitter: false,
+          yandex: false,
+          windows: false
+        }
+      }),
+    ],
   };
 };
