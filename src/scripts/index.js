2ca563aae719e1f624eaecffeb8437e5eaf9b7fa
Pavel Belugin <pashaigood@gmail.com>
2018-11-16 12:26:34 +0300

    docs(new placeholder):

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,2 @@
-import 'styles/index.css';
-import 'styles/index.scss';
-import 'styles/index.styl';
-import 'styles/index.less';
-
-open('//google.com');
-console.log(44234324);
+import '../styles';
+import './main';
